eda54b445e3190f25c630ac27fb79aabaed50e70
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-02-10 19:41:39 +0100

    add json parser

--- src/main/java/it/goff/np/mainScene/MainViewStageController.java
+++ src/main/java/it/goff/np/mainScene/MainViewStageController.java
@@ -4,8 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainViewStageController {
+    @FXML
+    private GridPane grid;
+
     @FXML
     private TextArea list;
 
@@ -13,5 +20,12 @@ public class MainViewStageController {
     private void click(ActionEvent event) {
         Button btn = (Button)event.getSource();
         list.setText(btn.getText());
+    }
+
+    void startButton() {
+        List<Button> btnList = Stream.generate(Button::new).limit(5).collect(Collectors.toList());
+        btnList.forEach(f -> f.setText("Ciao"));
+        grid.addRow(0, btnList.get(0));
+
     }
 }
